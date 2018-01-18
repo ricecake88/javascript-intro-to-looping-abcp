6eb11129e93497ab1dea820a1a73770abcfa7f3d
Grace Shih <vocalise@gmail.com>
2018-01-18 22:11:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,23 +1,10 @@
-/*function forLoop(myArray) {
+function forLoop(myArray) {
   myArray = [];
-  for (var i = 0; i < 25; i++)
+  for (var i = 0 + myArray.length; i < 25; i++)
   {
     myArray.push(`I am ${i+1} strange loop${i === 0 ? '':'s'}.`);
   }
   return myArray;
-}*/
-
-function forLoop(array){
-	array = [];
-  for (var i = 0; i<25; i++){
-    if(i===1){
-      array.push('I am 1 strange loop.')
-    }
-    else{
-      array.push(`I am ${i} loops.`)
-    }
-  }
-  return array
 }
 
 function whileLoop(number) {
