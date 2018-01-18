1d1d51b41f5bd158fe974776de19518f538058c9
Grace Shih <vocalise@gmail.com>
2018-01-18 18:18:34 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(myArray) {
+  for (var i = 0; i < 25; i++)
+  {
+    myArray[i] = `"I am ${i+1} strange loop${i === 0 ? '':'s'}."`;
+  }
+  console.log(myArray);
+  return myArray;
+}
+
+function whileLoop(number) {
+  while(number > 0) {
+    console.log(number--);
+  }
+  return "done";
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(myArray) {
+  console.log(maybeTrue());
+  do {
+    myArray.pop();
+  } while (maybeTrue() && (myArray.length > 0));
+  
+  console.log("In doWhileLoop " + myArray);
+  return myArray;
+}
+
