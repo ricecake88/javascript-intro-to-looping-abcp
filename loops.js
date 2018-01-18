f9853ce22dd714323bd9457b1f7c414d9fa861ef
Grace Shih <vocalise@gmail.com>
2018-01-18 22:24:03 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,24 +1,12 @@
-/*function forLoop(myArray) {
-  myArray = [];
-  for (var i = 0; i < 25; i++)
-  {
-    myArray.push(`I am ${i+1} strange loop${i === 0 ? '':'s'}.`);
+function forLoop(myArray) {
+  
+  for (var i = 0; i < 25; i++) {
+    myArray.push(`I am ${(i === 0)? 1 : i+1} strange loop${(i === 0)?'':'s'}`);
   }
+  console.log(myArray);
   return myArray;
-}*/
-
-function forLoop(array){
-	array = [];
-  for (var i = 0; i<25; i++){
-    if(i===1){
-      array.push('I am 1 strange loop.')
-    }
-    else{
-      array.push(`I am ${i} loops.`)
-    }
-  }
-  return array
 }
+
 
 function whileLoop(number) {
   while(number > 0) {
